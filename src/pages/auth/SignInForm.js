0a75a636a5b63d7430994f43507f86ab6a174da4
strasse34 <rmirzaie2021@gmail.com>
2024-03-25 13:59:52 +0000

    styles: made signin/up form centered

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -51,7 +51,9 @@ function SignInForm() {
   };
 
   return (
-    <Row className={styles.Row}>
+    <Row
+      className={`${styles.Row} justify-content-center align-items-center md-mt-5`}
+    >
       <Col className="my-auto p-0 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign in</h1>
